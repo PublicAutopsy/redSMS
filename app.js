ada56d7d13e151976e0546780bb5fab6aa78b526
Cody B <byrnes.cody@gmail.com>
2013-01-15 10:27:32 -0800

    Outputing first 5 in array

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ var express = require("express"),
 
 var app = express.createServer();
 
-app.all('/', function(req, res){
-	restler.get("http://reddit.com/.json").on('complete', function(reddit){
-		var titles ="";
-		for(var i=0; 1<5; i++){
-			titles += reddit.data.children[1].data.title;
+app.all('/:subreddit', function(req, res){
+	restler.get("http://www.reddit.com/r/"+ req.params.subreddit + ".json").on('complete', function(reddit){
+		var reddit_data = reddit.data.children;
+		var titles = new Array();
+		var i = 0;
+		while (i<5){
+			
+			titles[i] = i+": " + reddit_data[i].data.title;
+			i++
 		}
 		res.send(titles);
+		//console.log("Got it sending it out!");
+		//res.send(reddit_data[1].data.title);
 	});
 });
 
